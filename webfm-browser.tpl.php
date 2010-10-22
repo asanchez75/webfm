b4cd4f13012555dd0a332f60f921895e164a685a
nhck <n@nhck.de>
2010-10-22 22:16:45 +0000

    Code cleanup for webfm with http://drupal.org/project/coder

--- webfm-browser.tpl.php
+++ webfm-browser.tpl.php
@@ -2,14 +2,14 @@
 // $Id$
 
 /**
- * @file webfm_browser.tpl.php
- * Display the webfm browser.
+ * @file
+ *    Display the webfm browser.
  *
  * Variables:
  * - $links: debug, settings and help links according to permissions
  * - $upload: upload fieldset if the user is permitted access
- *
- * @see webfm_theme()
+ * 
+ * @see webfm_theme(), webfm-popup.tpl.php 
  */
 ?>
 <div class="more-help-link">
